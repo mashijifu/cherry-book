45223eb4d9c5e4fb0633f51ccb78399c56cc3502
mashijifu <4ts0t0i7c@gmail.com>
2024-05-06 09:48:15 +0900

    テスト結果がエラーになる検証コード

--- Section3/sample_test.rb
+++ Section3/sample_test.rb
@@ -2,8 +2,14 @@ require 'minitest/autorun'
 
 class SampleTest < Minitest::Test
   def test_sample
+    # テスト結果が成功する検証コード
     # assert_equal 'RUBY', 'ruby'.upcase
-    assert_equal 'RUBY', 'ruby'.capitalize
+
+    # テスト結果が失敗する検証コード
+    # assert_equal 'RUBY', 'ruby'.capitalize
+
+    # テスト結果がエラーになる検証コード
+    assert_equal 'RUBY', nil.upcase
   end
 end
 
