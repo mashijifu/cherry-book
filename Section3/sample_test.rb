93aeb6404aefdbc65e0114b72d41d3b254885548
mashijifu <4ts0t0i7c@gmail.com>
2024-05-06 09:43:16 +0900

    テストが失敗するコード

--- Section3/sample_test.rb
+++ Section3/sample_test.rb
@@ -2,7 +2,8 @@ require 'minitest/autorun'
 
 class SampleTest < Minitest::Test
   def test_sample
-    assert_equal 'RUBY', 'ruby'.upcase
+    # assert_equal 'RUBY', 'ruby'.upcase
+    assert_equal 'RUBY', 'ruby'.capitalize
   end
 end
 
