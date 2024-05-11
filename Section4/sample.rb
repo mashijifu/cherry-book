5b42aeb91355dbd2f34a759d79629834fbcd6b71
mashijifu <4ts0t0i7c@gmail.com>
2024-05-11 12:57:30 +0900

    find/detect

--- Section4/sample.rb
+++ Section4/sample.rb
@@ -125,3 +125,10 @@ numbers = [1, 2, 3, 4, 5, 6]
 # 3の倍数を除外する（3の倍数以外を集める）
 non_multiples_of_three = numbers.reject { |n| n % 3 == 0 }
 non_multiples_of_three #=> [1, 2, 4, 5]
+
+# find/detect
+# findメソッドはブロックの戻り値が真になった最初の要素を返す
+numbers = [1, 2, 3, 4, 5, 6]
+# ブロックの戻り値が最初に真になった要素を返す
+even_number = numbers.find { |n| n.even? }
+even_number #=> 2
