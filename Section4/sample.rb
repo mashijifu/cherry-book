e4f8b9e9126bcc9c38924ae53f4d673b5080bf31
mashijifu <4ts0t0i7c@gmail.com>
2024-05-12 12:33:37 +0900

    case文で範囲オブジェクトを使用

--- Section4/sample.rb
+++ Section4/sample.rb
@@ -241,3 +241,26 @@ liquid?(-1) #=> false
 liquid?(0) #=> true
 liquid?(99) #=> true
 liquid?(100) #=> false
+
+# case文で使う
+# 年齢に応じて料金を判定する
+def charge(age)
+  case age
+  # 0歳から5歳までの場合
+  when 0..5
+    0
+  # 6歳から12歳までの場合
+  when 6..12
+    300
+  # 13歳から18歳までの場合
+  when 13..18
+    600
+  # それ以外の場合
+  else
+    1000
+  end
+end
+charge(3) #=> 0
+charge(12) #=> 300
+charge(16) #=> 600
+charge(25) #=> 1000
