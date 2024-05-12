2c3b309b7d7061019dd2e049fb780ad01e32e595
mashijifu <4ts0t0i7c@gmail.com>
2024-05-12 12:40:24 +0900

    範囲オブジェクトで、繰り返し処理を行う

--- Section4/sample.rb
+++ Section4/sample.rb
@@ -274,3 +274,21 @@ charge(25) #=> 1000
 
 ('bad'..'bag').to_a #=> ["bad", "bae", "baf", "bag"]
 ('bad'...'bag').to_a #=> ["bad", "bae", "baf"]
+
+# 繰り返し処理を行う
+# 範囲オブジェクトを配列に変換してから繰り返し処理を行う
+numbers = (1..4).to_a
+sum = 0
+numbers.each { |n| sum += n }
+sum #=> 10
+
+# 範囲オブジェクトに対して直接eachメソッドを呼び出す
+sum = 0
+(1..4).each { |n| sum += n }
+sum #=> 10
+
+# stepメソッドを使うと、値を増やす間隔を指定できる
+numbers = []
+# 1から10まで2つ飛ばしで繰り返し処理をする
+(1..10).step(2) { |n| numbers << n }
+numbers #=> [1, 3, 5, 7, 9]
