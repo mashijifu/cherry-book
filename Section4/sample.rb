19f0e8a93cade259a1a9ca94c7cde5f1c601f6fb
mashijifu <4ts0t0i7c@gmail.com>
2024-05-12 12:30:27 +0900

    n以上m以下、n以上m未満の判定をする

--- Section4/sample.rb
+++ Section4/sample.rb
@@ -221,3 +221,23 @@ a[1..3] #=> [2, 3, 4]
 
 a = 'abcdef'
 a[1..3] #=> "bcd"
+
+# n以上m以下、n以上m未満の判定をする
+# 不等号を使う場合
+def liquid?(temperature)
+  # 0度以上100度未満で液体、と判定したい
+  0 <= temperature && temperature < 100
+end
+liquid?(-1) #=> false
+liquid?(0) #=> true
+liquid?(99) #=> true
+liquid?(100) #=> false
+
+# 範囲オブジェクトを使う場合
+def liquid?(temperature)
+  (0...100).include?(temperature)
+end(
+liquid?(-1) #=> false
+liquid?(0) #=> true
+liquid?(99) #=> true
+liquid?(100) #=> false
