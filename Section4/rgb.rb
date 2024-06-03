a998ad2455d9ca0ecb670a33fd06f0378730df27
mashijifu <4ts0t0i7c@gmail.com>
2024-06-03 22:59:58 +0900

    with_indexメソッドを使った添え字付きの繰り返し処理

--- Section4/rgb.rb
+++ Section4/rgb.rb
@@ -350,3 +350,20 @@ fruits.each_with_index { |fruit, i| puts "#{i}: #{fruit}" }
 # 0: apple
 # 1: orange
 # 2: melon
+
+# with_indexメソッドを使った添え字付きの繰り返し処理
+fruits = ['apple', 'orange', 'melon']
+# mapとして処理しつつ、添え字も受け取る
+fruits.map.with_index { |fruit, i| "#{i}: #{fruit}" }
+# => ["0: apple", "1: orange", "2: melon"]
+
+fruits = ['apple', 'orange', 'melon']
+# 名前に"a"を含み、かつ添え字が奇数である要素を削除する
+fruits.delete_if.with_index { |fruit, i| fruit.include?('a') && i.odd? }
+fruits # => ["apple", "melon"]
+
+fruits = ['apple', 'orange', 'melon']
+# ブロックなしでメソッドを呼ぶとEnumeratorオブジェクトが返る。よってwith_indexメソッドが呼び出せる
+p fruits.each # => #<Enumerator: ["apple", "orange", "melon"]:each>
+p fruits.map # => #<Enumerator: ["apple", "orange", "melon"]:map>
+p fruits.delete_if # => #<Enumerator: ["apple", "orange", "melon"]:delete_if>
