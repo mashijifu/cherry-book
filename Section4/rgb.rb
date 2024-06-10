4a0c81bf08b3773b9aada8c5db2263e5bd55b1f5
mashijifu <4ts0t0i7c@gmail.com>
2024-06-10 23:58:48 +0900

    throwとcatchを使った

--- Section4/rgb.rb
+++ Section4/rgb.rb
@@ -861,3 +861,23 @@ end
 #   orange, 2
 #   orange, 1
 #   orange, 3
+
+# throwとcatchを使った大域脱出
+fruits = ['apple', 'melon', 'orange']
+numbers = [1, 2, 3]
+catch :done do
+  fruits.shuffle.each do |fruit|
+    numbers.shuffle.each do |n|
+      puts "#{fruit}, #{n}"
+      if fruit == 'orange' && n == 3
+        # すべての繰り返し処理を脱出する
+        throw :done
+      end
+    end
+  end
+end
+#=> melon, 2
+#   melon, 1
+#   melon, 3
+#   orange, 1
+#   orange, 3
