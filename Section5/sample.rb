df17bfbe375c7667085c7a3fb95cc4352ba772fb
mashijifu <4ts0t0i7c@gmail.com>
2024-06-14 23:53:13 +0900

    要素の追加、変更、取得

--- Section5/sample.rb
+++ Section5/sample.rb
@@ -32,3 +32,27 @@ h = {}
 
 # 空のブロック（めったに使われない）
 [1, 2, 3].each {}
+
+# 要素の追加、変更、取得
+currencies = { "japan" => "yen", "us" => "dollar", "india" => "rupee" }
+
+# イタリアの通貨を追加する
+currencies["italy"] = "euro"
+
+currencies #=> { "japan" => "yen", "us" => "dollar", "india" => "rupee", "italy" => "euro" }
+
+# すでにキーが存在する場合は値が上書きされる
+currencies = { "japan" => "yen", "us" => "dollar", "india" => "rupee" }
+
+# 既存の値を上書きする
+currencies["japan"] = "円"
+
+currencies #=> { "japan" => "円", "us" => "dollar", "india" => "rupee" }
+
+currencies = { "japan" => "yen", "us" => "dollar", "india" => "rupee" }
+
+currencies["india"] #=> "rupee"
+
+currencies = { "japan" => "yen", "us" => "dollar", "india" => "rupee" }
+
+currencies["brazil"] #=> nil
